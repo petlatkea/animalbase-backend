ca87d275837fdb4cdbd6036ec8e64c074eddf698
Peter Lind (KEA) <petl@kea.dk>
2023-08-07 16:26:49 +0200

    GET id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs/promises';
 import cors from 'cors';
+import { log } from 'console';
 
 const filename = "animalbase2023.json";
 
@@ -18,4 +19,16 @@ app.get("/animals", async (req,res) => {
   const animals = JSON.parse(await fs.readFile(filename));
   console.log(animals);
   res.json(animals);
+});
+
+app.get("/animals/:id", async (req,res) => {
+  const id = req.params.id;
+  const animals = JSON.parse(await fs.readFile(filename));
+  const animal = animals.find(animal => animal.id == id);
+
+  if(animal) {
+    res.json(animal);
+  } else {
+    res.status(404).end();
+  }
 });
